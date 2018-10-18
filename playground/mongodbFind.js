3fd607ad62f7ca394a3c1bd311aad9b521d529b6
Sibrenlagauw <sibren@mlagauw.com>
2018-10-18 15:10:06 +0200

    Inital commit

---
+++ playground/mongodbFind.js
@@ -0,0 +1,19 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to mongodb server.');
+  }
+  console.log('Connected to mongodb server.');
+  const db = client.db('TodoApp');
+
+  db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('error happened');
+  });
+
+  //client.close();
+});
